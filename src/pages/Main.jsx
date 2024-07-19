7c126a41a7965418a215cd7bef7846ed7aecad2d
AB7zz <abhinavcv007@gmail.com>
2024-07-20 00:35:11 +0530

    men ubar animation

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -7,41 +7,18 @@ import TimeSchedule from '../slides/TimeSchedule'
 import About from '../slides/About'
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import './style.css'
 
 gsap.registerPlugin(ScrollTrigger);
 
 
 const Main = () => {
   const component = React.useRef();
-  const slider1 = React.useRef();
   const slider2 = React.useRef();
   const slider3 = React.useRef();
 
   React.useLayoutEffect(() => {
     let ctx = gsap.context(() => {
-      let panels1 = gsap.utils.toArray(".panel1");
       let pauseRatio = 0.1;
-      let t1 = gsap.timeline({
-        scrollTrigger: {
-          trigger: slider1.current,
-          pin: true,
-          scrub: 1,
-          snap: 1 / (panels1.length - 1),
-          end: () => "+=" + slider1.current.offsetWidth,
-          markers: false
-        }
-      });
-      t1.to(
-        panels1,
-        {
-          xPercent: -100 * (panels1.length - 1),
-          duration: 1,
-          ease: "none"
-        },
-        pauseRatio
-      );
-      t1.to({}, { duration: pauseRatio });
       
       let panels2 = gsap.utils.toArray(".panel2");
       
@@ -94,17 +71,17 @@ const Main = () => {
   return (
     <>
       <div ref={component}>
-        <div className="bg-yellow-500 h-[100vh]">
+        <div id="home" className="bg-[#050B17] h-[100vh]">
           <Home />
         </div>
 
         
-        <div ref={slider1} className="w-[600vw] h-screen flex flex-wrap">
+        <div id="events" className="w-[600vw] flex flex-wrap bg-[#050B17]">
           <Events />
         </div>
 
 
-        <div ref={slider2} className="w-[600vw] h-screen flex flex-wrap">
+        <div id="speakers" ref={slider2} className="relative z-2 w-[600vw] h-screen flex flex-wrap">
           <Speaker name={"Dr. Tessy Thomas1"} para1={"Her exceptional contributions to missile technology and defense systems have not only elevated India's capabilities but also inspired countless individuals, especially women, to pursue careers in science and technology. Dr. Thomas's keynote address promises to be an enlightening and inspiring start to our event, setting the stage for an incredible day of space exploration discussions and insights. Don't miss the opportunity to hear from this visionary leader at SpaceUp CUSAT on October 14, 2023."} para2={"We are honored to announce Dr. Tessy Thomas as the Chief Guest and our First Keynote Speaker for SpaceUp CUSAT 2023. Known as the 'Missile Woman of India,' Dr. Tessy Thomas has made an indelible mark in the field of aerospace and defense. As the former Director of the Advanced Systems Laboratory (ASL) and Project Director for Agni-IV, she played a pivotal role in India's strategic missile program. "} designation={"Director General of Aeronautical Systems Missile woman of India"}  />
           <Speaker name={"Dr. Tessy Thomas2"} para1={"Her exceptional contributions to missile technology and defense systems have not only elevated India's capabilities but also inspired countless individuals, especially women, to pursue careers in science and technology. Dr. Thomas's keynote address promises to be an enlightening and inspiring start to our event, setting the stage for an incredible day of space exploration discussions and insights. Don't miss the opportunity to hear from this visionary leader at SpaceUp CUSAT on October 14, 2023."} para2={"We are honored to announce Dr. Tessy Thomas as the Chief Guest and our First Keynote Speaker for SpaceUp CUSAT 2023. Known as the 'Missile Woman of India,' Dr. Tessy Thomas has made an indelible mark in the field of aerospace and defense. As the former Director of the Advanced Systems Laboratory (ASL) and Project Director for Agni-IV, she played a pivotal role in India's strategic missile program. "}  />
           <Speaker name={"Dr. Tessy Thomas3"} para1={"Her exceptional contributions to missile technology and defense systems have not only elevated India's capabilities but also inspired countless individuals, especially women, to pursue careers in science and technology. Dr. Thomas's keynote address promises to be an enlightening and inspiring start to our event, setting the stage for an incredible day of space exploration discussions and insights. Don't miss the opportunity to hear from this visionary leader at SpaceUp CUSAT on October 14, 2023."} para2={"We are honored to announce Dr. Tessy Thomas as the Chief Guest and our First Keynote Speaker for SpaceUp CUSAT 2023. Known as the 'Missile Woman of India,' Dr. Tessy Thomas has made an indelible mark in the field of aerospace and defense. As the former Director of the Advanced Systems Laboratory (ASL) and Project Director for Agni-IV, she played a pivotal role in India's strategic missile program. "}  />
@@ -112,18 +89,17 @@ const Main = () => {
         </div>
 
 
-        <div className='bg-green-500 flex h-[100vh]'>
+        <div id="timeschedule" className='relative z-2 bg-green-500 flex h-[100vh]'>
           <TimeSchedule />
         </div>
 
 
-        <div ref={slider3} className="bg-[#050B17] w-[600vw] h-screen flex flex-wrap">
+        <div id="previmages" ref={slider3} className="relative z-2 bg-[#050B17] w-[600vw] h-screen flex flex-wrap">
           <PrevImages />
-          
         </div>
 
 
-        <div className='bg-black py-10 md:h-[100vh]'>
+        <div id="about" className='relative z-2 bg-black py-10 md:h-[100vh]'>
           <About />
         </div>
 
