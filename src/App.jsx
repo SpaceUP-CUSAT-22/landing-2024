fb34f76d787e81e9a77d0e60bc16e93fdd43665d
AB7zz <abhinavcv007@gmail.com>
2024-10-04 22:56:58 +0530

    events evin code merged

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Main from "./pages/Main"
 import BuyMerch from './pages/BuyMerch'
-import Events from './events'
+
 function App() {
   React.useEffect(() => {
     
@@ -13,7 +13,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/buymerch" element={<BuyMerch />} />
-          <Route path="/events" element={<Events />} />
         </Routes>
       </BrowserRouter>
     </>
