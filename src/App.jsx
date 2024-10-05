70a33f8d0db342f3daa1e1b95d97ec80779c2dd9
AB7zz <abhinavcv007@gmail.com>
2024-10-05 22:32:58 +0530

    buyticket init

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Main from "./pages/Main"
 import BuyMerch from './pages/BuyMerch'
+import BuyTicket from './pages/BuyTicket'
 
 function App() {
   React.useEffect(() => {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/buymerch" element={<BuyMerch />} />
+          <Route path="/buyticket" element={<BuyTicket />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/BuyTicket.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BuyTicket = () => {
+  return (
+    <div className='text-white'>BuyTicket</div>
+  )
+}
+
+export default BuyTicket
