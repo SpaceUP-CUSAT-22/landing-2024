622753263d0425936fbfac12abdb6f9d2d8fc875
AB7zz <abhinavcv007@gmail.com>
2024-10-17 08:32:55 +0530

    26

--- src/components/Home/TimeSchedule.jsx
+++ src/components/Home/TimeSchedule.jsx
@@ -184,7 +184,7 @@ const TimeSchedule = () => {
         <div className="content flex flex-col mb-0">
           <div className="date-display font-alternox-regular mb-8 leading-tight text-white md:mb-12">
             <div className="text-lg md:text-xl">ON</div>
-            <div className="text-3xl md:text-4xl">20 OCT 2024</div>
+            <div className="text-3xl md:text-4xl">26 OCT 2024</div>
             <div className="text-lg md:text-xl">SUNDAY</div>
           </div>
 
