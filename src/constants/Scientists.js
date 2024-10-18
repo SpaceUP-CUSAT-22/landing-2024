028a1c80c5764418d51a3b69ed8227e58fe130b7
AB7zz <abhinavcv007@gmail.com>
2024-10-18 23:02:17 +0530

    sse

--- src/constants/Scientists.js
+++ src/constants/Scientists.js
@@ -1,7 +1,7 @@
 const scientists = [
     {
         name: "Dr. Murthy Remilia",
-        title: "Head of Project Management, ISRO",
+        title: "Head, Project Management, Gaganyaan Mission",
         location: "Bangalore, India",
         image: "/speakers/speaker1.png",
         description: "Dr. Murthy Remilia serves as the project head of the remarkable Gaganyaan mission. With a diverse 25-year career in technical, business, and management fields, his contributions to aerospace have been truly inspiring for students. His ability to excel in multiple areas simultaneously stands as a great motivation for aspiring college students."
