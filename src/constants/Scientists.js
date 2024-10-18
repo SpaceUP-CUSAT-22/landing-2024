887f186c1279cb7157c6c59cbcda737501e25c73
AB7zz <abhinavcv007@gmail.com>
2024-10-18 23:12:47 +0530

    tr

--- src/constants/Scientists.js
+++ src/constants/Scientists.js
@@ -7,18 +7,18 @@ const scientists = [
         description: "Dr. Murthy Remilla serves as the project head of the remarkable Gaganyaan mission. With a diverse 25-year career in technical, business, and management fields, his contributions to aerospace have been truly inspiring for students. His ability to excel in multiple areas simultaneously stands as a great motivation for aspiring college students."
     },
     {
-        name: "Dr. D Jagadheep",
-        title: "Associate Professor, IIST",
-        location: "Kerala, India",
-        image: "/speakers/speaker2.png",
-        description: "Dr. D Jagadheep is a professor of Earth Science and Space at the esteemed Indian Institute of Space Science and Technology (IIST). With many years of experience in teaching, his expertise in astronomy has led him to contribute to numerous projects, including the Max Planck Partner Group for Galactic Star Formation."
-    },
-    {
         name: "Dr. R. Satheesh Thampi",
         title: "Head of the Planetary Science Branch at the Space Physics Laboratory, Vikram Sarabhai Space Centre",
         location: "Thiruvananthapuram",
         image: "/speakers/speaker3.png",
         description: "Dr. R. Satheesh Thampi  leads the Planetary Science Branch at VSSC and is the Principal Investigator of the PAPA payload on the Aditya-L1 mission. His work significantly advances solar research, enhancing our understanding of solar phenomena, and underscores his dedication to space science and planetary exploration."
+    },
+    {
+        name: "Dr. D Jagadheep",
+        title: "Associate Professor, IIST",
+        location: "Kerala, India",
+        image: "/speakers/speaker2.png",
+        description: "Dr. D Jagadheep is a professor of Earth Science and Space at the esteemed Indian Institute of Space Science and Technology (IIST). With many years of experience in teaching, his expertise in astronomy has led him to contribute to numerous projects, including the Max Planck Partner Group for Galactic Star Formation."
     },
     {
         name: "Nikhitha Chadde",
